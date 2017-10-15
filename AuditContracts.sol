44e24819ab04b033602ba505dce754f50dd0e2d7
Pavel Ivanov <grammka@gmail.com>
2017-10-14 20:27:09 -0400

    changes

--- AuditContracts.sol
+++ AuditContracts.sol
@@ -28,20 +28,14 @@ contract StatsExtension {
 
 contract CompanyFactory is PermissionExtension {
 
-  address owner;
-
-  function CompanyFactory() {
-    owner = msg.sender;
-  }
+  address owner = msg.sender;
 
   // msg.sender => CompanyAddress
   mapping (address => address) public companies;
 
-  function createCompany(bytes32 name, string fileHash) returns (address) {
-    address company = new Company(msg.sender, name, fileHash);
-    companies[msg.sender] = company;
-    setAdmin(company);
-    return company;
+  function createCompany(bytes32 name, string fileHash) {
+    companies[msg.sender] = new Company(name, msg.sender, fileHash);
+    setAdmin(companies[msg.sender]);
   }
 }
 
@@ -50,45 +44,40 @@ contract Company is PermissionExtension {
 
   bytes32 public name;
   address public owner;
-  address[] public codes; // [codeAddress]
-  uint256 public codeCount;
-  string fileHash;
+  address[] public screenings; // [codeAddress]
+  uint256 public screeningCount;
+  string public fileHash;
 
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
   }
 
-  event Foo(uint8);
-
-  function Company(address _owner, bytes32 _name, string _fileHash) {
-    Foo(_fileHash);
-    owner = _owner;
+  function Company(bytes32 _name, address _owner, string _fileHash) {
     name = _name;
+    owner = _owner;
     fileHash = _fileHash;
   }
 
-  event Log(uint);
-
-  function addCode(string keyForCode, string behavior, string agenda, uint256 critical, uint256 major, uint256 minor, uint256 bountyAmount) onlyOwner payable {
-    codeCount += 1;
-    address newCodeAddress = new Code(keyForCode, behavior, agenda, critical, major, minor, owner, bountyAmount);
-    codes.push(newCodeAddress);
-    setAdmin(newCodeAddress);
-    setPerm(newCodeAddress, "Code");
+  function addScreening(string keyForScreening, string behavior, string agenda, uint256 critical, uint256 major, uint256 minor, uint256 bountyAmount) onlyOwner payable {
+    screeningCount += 1;
+    address newScreeningAddress = new Screening(keyForScreening, behavior, agenda, critical, major, minor, owner);
+    screenings.push(newScreeningAddress);
+    setAdmin(newScreeningAddress);
+    setPerm(newScreeningAddress, "Screening");
   }
 }
 
 
-contract Code is PermissionExtension {
+contract Screening is PermissionExtension {
 
   struct Rewards {
-    uint256 critical;
-    uint256 major;
-    uint256 minor;
+  uint256 critical;
+  uint256 major;
+  uint256 minor;
   }
 
-  string public keyForCode;
+  string public keyForScreening;
   string public behavior;
   string public agenda;
 
@@ -102,6 +91,9 @@ contract Code is PermissionExtension {
   bool public isOpen;
 
   address owner;
+
+  mapping (address => uint256) openClaimsIndexes;
+  mapping (address => uint256) claimsIndexes;
   address[] public openClaims; // ?
   address[] public claims; // ?
 
@@ -116,12 +108,12 @@ contract Code is PermissionExtension {
   //    bountyAmount = msg.value;
   //}
 
-  function Code(string _keyForCode, string _behavior, string _agenda, uint256 _critical, uint256 _major, uint256 _minor, address _owner, uint256 _bountyAmount) {
+  function Screening(string _keyForScreening, string _behavior, string _agenda, uint256 _critical, uint256 _major, uint256 _minor, address _owner) {
 
-    keyForCode = _keyForCode;
+    keyForScreening = _keyForScreening;
     behavior = _behavior;
     agenda = _agenda;
-    bountyAmount = _bountyAmount;
+    //bountyAmount = _bountyAmount;
     rewards.critical = _critical;
     rewards.major = _major;
     rewards.minor = _minor;
@@ -147,10 +139,12 @@ contract Code is PermissionExtension {
 
     require ((sumOfBountyForClaims + potentialReward) < bountyAmount);
     address newClaimAddress;
-    newClaimAddress =  new Claim(lineStart, lineEnd, comment, category, msg.sender, owner);
+    newClaimAddress =  new Claim(lineStart, lineEnd, comment, category, msg.sender, owner, potentialReward);
     openClaims.push(newClaimAddress);
+    openClaimsIndexes[newClaimAddress] = openClaimCount;
     openClaimCount += 1;
     claims.push(newClaimAddress); //?
+    claimsIndexes[newClaimAddress] = openClaimCount;
     claimCount += 1;
     setPerm(newClaimAddress, "Claim");
   }
@@ -164,11 +158,11 @@ contract Code is PermissionExtension {
     isOpen = false;
   }
 
-  // delete claim
-  function closeClaim(address closingClaimAddress) internal {
-    // to do check permissions
-    //openClaims.delete();
-    claimCount -= 1;
+  function closeClaim() {
+    require(checkPerm(msg.sender, "Claim"));
+    delete openClaims[openClaimsIndexes[msg.sender]];
+    openClaims.length--;
+    openClaimCount -= 1;
   }
 }
 
@@ -176,8 +170,8 @@ contract Code is PermissionExtension {
 contract Claim is PermissionExtension, StatsExtension {
 
   struct LineRange{
-    uint256 lineStart;
-    uint256 lineEnd;
+  uint256 lineStart;
+  uint256 lineEnd;
   }
 
   LineRange public lineRange;
@@ -188,41 +182,46 @@ contract Claim is PermissionExtension, StatsExtension {
   bool isOpen;
   bool public declined;
   address owner;
-  address codeOwner;
+  address screeningOwner;
+  address screeningAddress;
+  uint256 potentialReward;
 
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
   }
 
-  function Claim(uint256 _lineStart, uint256 _lineEnd, string _comment, bytes32 _category, address _owner, address _codeOwner) {
+  function Claim(uint256 _lineStart, uint256 _lineEnd, string _comment, bytes32 _category, address _owner, address _screeningOwner, uint256 _potentialReward) {
     lineRange.lineStart = _lineStart;
     lineRange.lineEnd = _lineEnd;
     comment = _comment;
     category = _category;
     owner = _owner;
-    codeOwner = _codeOwner;
+    screeningOwner = _screeningOwner;
+    screeningAddress = msg.sender;
+    potentialReward = _potentialReward;
   }
 
   function acceptBug() {
-    require(msg.sender == codeOwner);
+    require(msg.sender == screeningOwner);
+
   }
 
   function declineBug(bytes32 _declinesComment) { // ?
-    require(msg.sender == codeOwner);
+    require(msg.sender == screeningOwner);
     declinesComment = _declinesComment;
     declined = true;
   }
-
-  function cancelClaim() onlyOwner {
-    isOpen = false;
-    // delete claim at Code address
-  }
-
+  /*
+    function cancelClaim() onlyOwner {
+      isOpen = false;
+      // delete claim at Screening address
+    }
+  */
   function acceptAnswer() onlyOwner {
     require(declined);
+    Screening(screeningAddress).closeClaim();
     // to do return money to customer
-
   }
 
   /*
