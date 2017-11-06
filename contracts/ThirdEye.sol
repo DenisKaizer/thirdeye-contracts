64b6f091a51949a5fa7d91d56e7734d7b311bffb
dom <1kaizerd@gmail.com>
2017-11-06 04:44:49 +0300

    create Claims Claim + getClaims + getScreenings

--- contracts/ThirdEye.sol
+++ contracts/ThirdEye.sol
@@ -68,6 +68,10 @@ contract ScreeningFactory {
   function getScreeningLength() view returns(uint) {
     return screenings.length;
   }
+
+  function getScreenings() view returns(address[]) {
+    return screenings;
+  }
 }
 
 contract Screening is Ownable {
@@ -86,6 +90,9 @@ contract Screening is Ownable {
   bytes32 descriptionHash;
 
   address[] public openClaims;
+  mapping (address => uint) public claimsIndex;
+  mapping (address => uint[]) public ownerClaims;
+
   address public factory;
 
   Rewards rewards;
@@ -93,10 +100,12 @@ contract Screening is Ownable {
   bool public screeningActive;
 
   modifier notOpenClaims() {
+    require(openClaims.length <= 1);
     _;
   }
 
   modifier onlyClaim() {
+    require(claimsIndex[msg.sender] >= 0);
     _;
   }
 
@@ -120,10 +129,15 @@ contract Screening is Ownable {
     rewards.majorReward = _majorReward;
     rewards.criticalReward = _criticalReward;
     screeningActive = true;
+    openClaims.push(0x0);
   }
 
   function () payable{
     totalAmount = msg.value;
+  }
+
+  function getClaims() view returns(address[]) {
+    return openClaims;
   }
 
   function pauseScreening() onlyOwner {
@@ -148,10 +162,88 @@ contract Screening is Ownable {
     totalAmount += msg.value;
   }
 
-  event reportCreating();
+  function payReward(address reviwer, uint valueToPay) onlyClaim {
+    bool isSent = reviwer.call.gas(3000000).value(valueToPay)();
+    require(isSent);
+    uint index = claimsIndex[msg.sender];
+    delete openClaims[index];
+  }
 
-  function  createReport() public {
-    reportCreating;
+  event claimCreating(address);
+
+  function  createClaim(
+    uint8 category,
+    bytes32 comment,
+    uint lineNumber)
+    public
+  {
+    require(this.balance >= rewards.minorReward);
+    uint potentialReward = rewards.minorReward;
+    if (category == 2) {
+      require(this.balance >= rewards.majorReward);
+      potentialReward = rewards.majorReward;
+    }
+    else {
+      if (category == 3) {
+        require(this.balance >= rewards.criticalReward);
+        potentialReward = rewards.criticalReward;
+      }
+    }
+    address claim = new Claim (
+    msg.sender,
+    category,
+    comment,
+    lineNumber,
+    owner,
+    potentialReward);
+
+    openClaims.push(claim);
+    claimsIndex[claim] = openClaims.length - 1;
+    ownerClaims[msg.sender].push(openClaims.length - 1);
+    claimCreating(claim);
+  }
+}
+
+contract Claim is Ownable {
+
+  uint lineNumber;
+  bytes32 comment;
+  uint8 category;
+  address screeningOwner;
+  address screening;
+  uint potentialReward;
+
+  function Claim (
+    address _owner,
+    uint8 _category,
+    bytes32 _comment,
+    uint _lineNumber,
+    address _screeningOwner)
+  {
+    owner = _owner;
+    lineNumber = _lineNumber;
+    comment = _comment;
+    category = _category;
+    screeningOwner = _screeningOwner;
+    potentialReward = _potentialReward;
+    screening = msg.sender;
+  }
+
+  modifier onlyScreeningOwner() {
+    require(msg.sender == screeningOwner);
+    _;
+  }
+
+  function accept() onlyScreeningOwner {
+    Screening(screening).payReward(owner, potentialReward);
+  }
+
+  function reject() onlyScreeningOwner {
+
+  }
+
+  function cancel() onlyOwner {
+
   }
 }
 
